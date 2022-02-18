ed802a085fd38772e7e176f8868d2c787c59705c
Bruno Roja <brunojlee@hotmail.com>
2022-02-18 19:52:48 -0400

    Requisito 8 finalizado

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Title from './Title';
 
 class Missions extends React.Component {
   render() {
     return (
-      <div className="card text-center" data-testid="missions" />
+      <div className="card text-center" data-testid="missions">
+        <Title headline="Missões" />
+      </div>
     );
   }
 }
